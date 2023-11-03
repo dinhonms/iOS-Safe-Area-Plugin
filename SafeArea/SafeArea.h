48328230c94ecb5f9d447d1109f40d9c24db1100
dinhonms <dinhonms@gmail.com>
2023-11-03 13:33:57 -0300

    Initial Commit

---
+++ SafeArea/SafeArea.h
@@ -0,0 +1,18 @@
+//
+//  SafeArea.h
+//  SafeArea
+//
+//  Created by Nonato Sousa on 03/11/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SafeArea.
+FOUNDATION_EXPORT double SafeAreaVersionNumber;
+
+//! Project version string for SafeArea.
+FOUNDATION_EXPORT const unsigned char SafeAreaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SafeArea/PublicHeader.h>
+
+
